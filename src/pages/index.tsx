8f52eb3099c2c416380fbf1a0a31c19d7b0dd476
dhruvil shah <dhruvilshah884@gmail.com>
2025-03-13 09:36:31 +0530

    --

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const index = () => {
   return (
     <div>
-      <h1>Hello</h1>
+      <h1>Hello Welcome To The Gate-Pass Api For More Information Go to Documentation</h1>
     </div>
   )
 }
