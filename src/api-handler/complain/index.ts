31549df4e940255d4a674dc09154cbb9f2a08ce6
dhruvil <dhruvilshah884@gmail.com>
2025-04-16 12:25:43 +0530

    complaint changes

--- src/api-handler/complain/index.ts
+++ src/api-handler/complain/index.ts
@@ -1,27 +1,28 @@
 import axios from 'axios'
 
 export interface IComplain {
-    pageSize?: number
-    page?: number
-    q?: string
+  pageSize?: number
+  page?: number
+  q?: string
 }
 
 export const fetchComplains = async (params: IComplain) => {
-    try {
-        const response = await axios.get('/api/complaint', {
-            headers: {
-                Authorization: `Bearer ${localStorage.getItem('token')}`
-            }
-        })
-        return {
-            success: true,
-            data: response.data
-        }
-    } catch (error) {
-        return {
-            success: false,
-            data: null,
-            message: (error as any).message
-        }
+  try {
+    const response = await axios.get('/api/complaint', {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('token')}`
+      },
+      params
+    })
+    return {
+      success: true,
+      data: response.data
     }
+  } catch (error) {
+    return {
+      success: false,
+      data: null,
+      message: (error as any).message
+    }
+  }
 }
